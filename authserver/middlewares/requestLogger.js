79d8513677dede138673a2eb7987ecf544f39d13
Andreas Levander <andreas.levander@abo.fi>
2022-07-20 08:48:17 +0300

    removed hard added authserver

--- authserver/middlewares/requestLogger.js
+++
@@ -1,9 +0,0 @@
-import logger from "../utils/logger.js";
-
-const loggerMiddleware = (request, response, next) => {
-  logger.http(`${request.method} - ${request.path} - ${request.ip}`);
-
-  next();
-};
-
-export default loggerMiddleware;
